62b666a85c83313e8a2f68af515b2b9a94f20add
gukiruthika <112635198+gukiruthika@users.noreply.github.com>
2022-09-08 13:29:46 +0530

    Add files via upload

---
+++ GrandChildren.java
@@ -0,0 +1,29 @@
+package sept8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GrandChildren {
+
+	public static void main(String[] args) {
+		Scanner sn = new Scanner(System.in);
+		String[][] names = {{"luke","shaw"},{"wayne","rooney"},{"rooney","ronaldo"},{"shaw","rooney"}};
+		String in = sn.next();
+		int count=0;
+		for(int i=0; i<names.length; i++) {
+			if(names[i][1].equalsIgnoreCase(in)) {
+				for(int j=0; j<names.length; j++) {
+					if(names[j][1].equalsIgnoreCase(names[i][0])) {
+						count++;
+					}
+				}
+			}
+		}
+		if(count>1)
+			System.out.println(in + " has " + count + " grandchildren.");
+		else
+			System.out.println(in + " has " + count + " grandchild.");
+		sn.close();
+	}
+
+}
